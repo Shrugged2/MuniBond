f19e91344a974d6e2820407bea8dbfc270bbb448
Rick <rcwilson262@gmail.com>
2022-06-23 21:20:35 -0500

    update

--- Contracts/main_publisher.py
+++ Contracts/main_publisher.py
@@ -305,12 +305,13 @@ period = 10 # how many blocks between two interest payments
 span = 500 # the length of interest payment period (exaggerated for demo purpose)
 client = algod_client()
 
-# how do I add in here, looking for something that references current ALGO/USD?
 # looking essentially for a price oracle. how do I do that here?
 
 # Further notes.  
 #     Would like to add in native time. IE "every 6 months"
-# how to reference a specific time? Again, reference something off chain? is the best solution here to set up own oracle?
+# how to reference a specific time? 
+# Again, reference something off chain? 
+# is the best solution here to set up own oracle? 
 #--------------------------------------------------------------------
 # ENSURING FIRST INTEREST PAYMENT IS A MULTIPLE OF PERIOD
 print("Checking configurations......")
--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 Overall, working on three technical aspects of this. 
 
-1) contracts that work in the backgound
-2) front end setup and creation page
-3) marketplace
+1) contracts that work in the backgound. token creation and persistence. reference to price and time standards
+2) front end setup and creation page. interaction with wallets
+3) marketplace. there is a skeleton. need to build out the whole body
 
 ![Flow of a bond](https://user-images.githubusercontent.com/42774042/174642625-d03dcbbe-cd7c-4e65-a392-b6168e80fa6e.png)
